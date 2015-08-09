fe340940d51a1e1ca64e008fb2476927f32b5fbb
Caleb Hicks <calebhicks@gmail.com>
2015-08-08 20:28:18 -0600

    adds data controller to delete it

---
+++ Festivus/Festivus/DataController.h
@@ -0,0 +1,15 @@
+//
+//  DataController.h
+//  Festivus
+//
+//  Created by Caleb Hicks on 8/8/15.
+//  Copyright © 2015 Alan Barth. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataController : NSObject
+
++ (void)checkForUpdatedData;
+
+@end
